5e1d80964cde0b30e29e1bece912399cea1a5261
Wei Lun <weilun_95@hotmail.com>
2018-03-20 17:31:05 +0800

    create basic view and model

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="col-md-6 col-lg-6 col-md-offset-3  col-lg-offset-3">
+  <div class="panel panel-primary">
+  <div class="panel-heading">Movies <a class="pull-right btn btn-primary btn-sm "href="/movies/create">Add New Movie</a></div>
+    <div class="panel-body">
+      <ul class="list-group">
+      @foreach($movies as $movie)
+      <li class="list-group-item"> <a href="/companies/{{ $company->id }}">{{ $movie->title}}</a> </li>
+      @endforeach
+      </ul>
+    </div>
+  </div>
+</div>
+@endsection
